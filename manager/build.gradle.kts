c4c275363b05dfc2e5c2c3e92e3c414ecfbc6fab
Luoxi Builder <luoxi@toolbox.local>
2026-08-16 16:50:50 +0000

    fix: 内部版本号改为 1000000，版本名 1.0.0

--- manager/build.gradle.kts
+++ manager/build.gradle.kts
@@ -25,10 +25,9 @@ fun getGitDescribe(): String {
 }
 
 fun getVersionCode(): Int {
-    val commitCount = getGitCommitCount()
-    return 30000 + commitCount
+    return 1000000
 }
 
 fun getVersionName(): String {
-    return getGitDescribe()
+    return "1.0.0"
 }
