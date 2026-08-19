86e0d8875bd2546377658ef95483a01292a6e679
xingyeNB886 <xingyeNB886@users.noreply.github.com>
2026-08-19 10:18:12 +0000

    ci: 加阿里云 Maven 镜像，绕过 Google Maven 仓库连接失败

--- manager/settings.gradle.kts
+++ manager/settings.gradle.kts
@@ -4,6 +4,9 @@ enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
 pluginManagement {
     repositories {
+        maven("https://maven.aliyun.com/repository/google")
+        maven("https://maven.aliyun.com/repository/public")
+        maven("https://maven.aliyun.com/repository/gradle-plugin")
         google()
         mavenCentral()
     }
@@ -11,6 +14,9 @@ pluginManagement {
 
 dependencyResolutionManagement {
     repositories {
+        maven("https://maven.aliyun.com/repository/google")
+        maven("https://maven.aliyun.com/repository/public")
+        maven("https://maven.aliyun.com/repository/gradle-plugin")
         google()
         mavenCentral()
         maven("https://jitpack.io")
